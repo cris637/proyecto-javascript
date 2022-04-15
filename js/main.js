0fcdf1b248330f55461a21b4910680e81ddbcbbe
cris637 <65936408+cris637@users.noreply.github.com>
2022-04-15 19:23:25 -0300

    Agregado de link de WhatsApp funcional

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let carrito = [];
 const pintarProductosAppend = document.getElementById("pintarProductosArray"); // Seleccionamos el contenedor donde se pintarán los productos
 const contenedorCarrito = document.querySelector("#lista-carrito tbody"); // Seleccionamos el contenedor de los productos del carrito
 const vaciarCarritp = document.getElementById("vaciar-carrito");
-const enviarCarrito = document.getElementById("enviar-carrito");
+const finalizar = document.getElementById("finalizar");
 
 const arrProductos = [];
 
@@ -138,21 +138,12 @@ function pintarCarrito(array, contenedor) {
             </td>
         `;
     contenedor.append(card);
-
   // enviar carrito
-    let enviar = document.createElement("a");
-    enviar.innerHTML = `
-            
-                <a href="https://api.whatsapp.com/send?phone=212121&text=Hola%20me%20gustaria%20hacer%20un%20pedido%20con%20las%20siguientes%20cosas ${item.nombre} ${item.cantidad}" class="btn btn btn-success"">Enviar Carrito</a>
-        `;
-    contenedor.append(enviar);
-
   }
 
   borrarProducto(); // Agregamos la funcion de borrar productos del carrito
   carritoVacioNoti(); // Agreamos la funcion de notificación de carrito vacío
   saveStorage(); // Storage
-  console.log(carrito)
 }
 
 
@@ -188,6 +179,7 @@ function borrarProducto() {
 function carritoVacioNoti() {
   if (carrito.length == 0) {
     vaciarCarritp.style.display = "none";
+    finalizar.style.display = "none";
   } else {
     // Ponerle flex a vaciarCarritp con todo centrado
     vaciarCarritp.style.display = "flex";
@@ -196,6 +188,13 @@ function carritoVacioNoti() {
     vaciarCarritp.style.flexDirection = "column";
     vaciarCarritp.style.width = "95%";
     vaciarCarritp.style.margin = "1rem auto";
+
+    finalizar.style.display = "flex";
+    finalizar.style.alignItems = "center";
+    finalizar.style.justifyContent = "center";
+    finalizar.style.flexDirection = "column";
+    finalizar.style.width = "95%";
+    finalizar.style.margin = "1rem auto";
   }
 }
 
@@ -249,3 +248,40 @@ function loadStorage(name) {
 
 
 
+finalizar.addEventListener("click", () => {
+  if (carrito.length == 0) {
+    Toastify({
+      text: "Carrito vacio",
+      duration: 1000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #951111 , #951111)",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+  } else {
+    saveStorage("carrito");
+    enviar(carrito);
+  }
+});
+
+function enviar(carrito) {
+  /*
+    Crear un enlace de whatsapp y pasar los productos del carrito convertidos a string por parámetro en el link https://api.whatsapp.com/send?phone=5611042722
+  */
+  let link = `https://api.whatsapp.com/send?phone=5493865394041&text=Hola%20me%20gustaría%20hacer%20un%20pedido%20con%20las%20siguientes%20cosas %0A`;
+  for (const producto of carrito) {
+    // Pasar los parámetros de cada producto a texto
+    const productoString = `${producto.cantidad}%20unidades de ${producto.nombre} con costo por prenda de $${producto.precio},`;
+
+    // Por cada producto en el carrito se añadirá el producto a la url
+    link += `${productoString}%0A`;
+  }
+  window.open(link, "_blank");
+}
+
+
+{/* <a href="https://api.whatsapp.com/send?phone=212121&text=Hola%20me%20gustaria%20hacer%20un%20pedido%20con%20las%20siguientes%20cosas ${item.nombre} ${item.cantidad}" class="btn btn btn-success"">Enviar Carrito</a> */}
